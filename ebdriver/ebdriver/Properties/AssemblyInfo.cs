9609236de069c6ea77eaa74cd7632bbcafd6bb9f
Yankovskiy <yankovskij98@mail.ru>
2018-10-22 13:56:16 +0300

    add_WebDriver

---
+++ ebdriver/ebdriver/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ebdriver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ebdriver")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a0b2aae-6f3e-4dae-a067-dbdfbd8e1fff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ebdriver/ebdriver/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium.Chrome;
+
+using OpenQA.Selenium;
+
+using OpenQA.Selenium.Remote;
+
+using OpenQA.Selenium.Support;
+
+using OpenQA.Selenium.Support.UI;
+
+using System.Threading;
+
+using System.Globalization;
+
+namespace ebdriver
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void inability_to_order_past_date()
+
+        {
+
+            IWebDriver Driver = new ChromeDriver();
+            DateTime date = DateTime.Now;
+
+
+
+            string today_date = date.ToString("dd.mm.yyyy");
+            Driver.Navigate().GoToUrl("https://www.onetwotrip.com/");
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//a[@href='/ru/']")));
+            Driver.FindElement(By.XPath("//a[@href='/ru/']")).Click();
+
+
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//li[@class ='searchFormABWayControlItem oneway']")));
+            Driver.FindElement(By.XPath("//li[@class ='searchFormABWayControlItem oneway']")).Click();
+
+
+          
+
+
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//input[@name ='from0']")));
+            Driver.FindElement(By.XPath("//input[@name ='from0']")).Click();
+            Driver.FindElement(By.XPath("//input[@name ='from0']")).SendKeys("Минск");
+
+            Driver.FindElement(By.XPath("//input[@name ='to0']")).Click();
+            Driver.FindElement(By.XPath("//input[@name ='to0']")).SendKeys("Москва");
+
+            Driver.FindElement(By.XPath("//input[@name ='date0']")).Click();
+            Driver.FindElement(By.XPath("//input[@name ='date0']")).SendKeys(today_date);
+
+
+            Driver.FindElement(By.XPath("//input[@name ='submit']")).Click();
+
+            //wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//div[@class='_1tBwH']")));
+           //Driver.FindElement(By.XPath("//div[@class='_1tBwH']")).Click();
+
+            Driver.Quit();
+
+        }
+    }
+}
