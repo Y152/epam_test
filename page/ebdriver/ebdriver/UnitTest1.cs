ba17f8609517bb24fc2305d21e144aa6b7deea82
Yankovskiy <yankovskij98@mail.ru>
2018-10-29 17:23:41 +0300

    add lab_5

---
+++ page/ebdriver/ebdriver/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenQA.Selenium.Chrome;
+
+using OpenQA.Selenium;
+
+using OpenQA.Selenium.Remote;
+
+using OpenQA.Selenium.Support;
+
+using OpenQA.Selenium.Support.UI;
+
+using System.Threading;
+
+using System.Globalization;
+
+namespace ebdriver
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        // Выбор одного города в качестве места отправления и прибытия
+
+      
+
+        public void TestMethod1()
+
+        {
+
+            IWebDriver driver = new ChromeDriver();
+
+            sity enter = new sity(driver);
+
+            HomePage homePage = new HomePage(driver);
+
+            homePage.goToPage();
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3));
+            driver.FindElement(By.XPath("//a[@href='/ru/']")).Click();
+
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//li[@class ='searchFormABWayControlItem oneway']")));
+
+            driver.FindElement(By.XPath("//li[@class ='searchFormABWayControlItem oneway']")).Click();
+
+
+
+
+
+            enter.Enter_dcity("Минск");
+
+
+     enter.Enter_acity("Минск");
+
+            enter.Enter_date();
+
+       
+
+            enter.Enter_result_click();
+
+            Assert.AreEqual(driver.FindElement(By.ClassName("dropdown-item-empty")).Text, "Unfortunately, we do not fly to/from Minsk");
+
+
+
+            driver.Close();
+
+            driver.Quit();
+
+        }
+    }
+}
---
+++ page/ebdriver/ebdriver/pages/sity.cs
@@ -0,0 +1,156 @@
+﻿using OpenQA.Selenium;
+
+using OpenQA.Selenium.Support.PageObjects;
+
+using System;
+
+
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+
+using OpenQA.Selenium.Chrome;
+
+
+
+using OpenQA.Selenium;
+
+
+
+using OpenQA.Selenium.Remote;
+
+
+
+using OpenQA.Selenium.Support;
+
+
+
+using OpenQA.Selenium.Support.UI;
+
+
+
+using System.Threading;
+
+
+
+using System.Globalization;
+
+
+
+
+namespace ebdriver
+
+{
+
+    public class sity
+
+    {
+
+        private IWebDriver driver;
+        DateTime date = DateTime.Now;
+        
+
+
+        By d_city = By.XPath("//input[@name ='from0']");
+
+        By a_city = By.XPath("//input[@name ='to0']");
+
+        By t_date = By.XPath("//input[@name ='date0']");
+
+        By enter_result = By.XPath("//input[@name ='submit']");
+
+
+
+        public sity(IWebDriver driver)
+
+        {
+
+            this.driver = driver;
+
+            PageFactory.InitElements(driver, this);
+
+        }
+
+
+
+        public void Enter_dcity(string city)
+
+        {
+
+            driver.FindElement(d_city).Clear();
+
+            driver.FindElement(d_city).SendKeys(city);
+
+        }
+
+
+
+        public void Enter_acity(string city)
+
+        {
+
+            driver.FindElement(a_city).Clear();
+
+            driver.FindElement(a_city).SendKeys(city);
+
+        }
+        public void Enter_date()
+
+        {
+
+            driver.FindElement(t_date).Clear();
+
+            driver.FindElement(t_date).SendKeys(date.ToString("dd.mm.yyyy"));
+        }
+
+
+
+        public void Enter_result_click()
+
+        {
+
+            driver.FindElement(enter_result).Click();
+
+        }
+
+    }
+
+
+
+    public class HomePage
+
+    {
+
+
+
+        private IWebDriver driver;
+
+
+
+        public HomePage(IWebDriver driver)
+
+        {
+
+            this.driver = driver;
+
+            PageFactory.InitElements(driver, this);
+
+        }
+
+
+
+        public void goToPage()
+
+        {
+
+            driver.Navigate().GoToUrl("https://www.onetwotrip.com/");
+
+        }
+
+
+
+    }
+
+}
+
